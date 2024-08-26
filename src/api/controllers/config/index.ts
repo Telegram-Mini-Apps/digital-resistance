4061e58719e1ffdadc32e614a98f39f4a671fd81
Dimitreee <danewdima@gmail.com>
2024-08-26 15:50:01 +0300

    remove useless code, remove csrf token protection

--- src/api/controllers/config/index.ts
+++
@@ -1,7 +0,0 @@
-import { Express } from 'express';
-import { csrfTokenMiddleware, verifyCsrfToken } from '../../middleware/csrfProtection';
-import { get as getConfig } from './route';
-
-export const attachConfigController = (app: Express) => {
-  app.get('/api/config', verifyCsrfToken, getConfig);
-};
