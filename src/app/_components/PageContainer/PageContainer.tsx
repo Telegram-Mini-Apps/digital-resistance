0d70925549acda379662373f9a025978ba912b60
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-26 06:09:31 +0500

    Remove PageContainer

--- src/app/_components/PageContainer/PageContainer.tsx
+++
@@ -1,7 +0,0 @@
-import { forwardRef, PropsWithChildren } from 'react';
-
-import styles from './PageContainer.module.scss';
-
-export const PageContainer = forwardRef<HTMLElement, PropsWithChildren>((props, ref) => {
-  return <main ref={ref} className={styles.root} {...props}/>;
-});
