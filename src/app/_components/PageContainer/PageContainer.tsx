a03db58d18a47492449ef95820a721132bf7437e
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-26 05:59:57 +0500

    Use forwardRef for PageContainer

--- src/app/_components/PageContainer/PageContainer.tsx
+++ src/app/_components/PageContainer/PageContainer.tsx
@@ -1,7 +1,7 @@
-import type { PropsWithChildren } from 'react';
+import { forwardRef, PropsWithChildren } from 'react';
 
 import styles from './PageContainer.module.scss';
 
-export function PageContainer(props: PropsWithChildren) {
-  return <main className={styles.root} {...props}/>;
-}
+export const PageContainer = forwardRef<HTMLElement, PropsWithChildren>((props, ref) => {
+  return <main ref={ref} className={styles.root} {...props}/>;
+});
