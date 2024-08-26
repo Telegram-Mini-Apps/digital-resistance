ed972bfd566991c9657e8c843abbea5424ed1a1e
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-26 05:12:53 +0500

    Make i18n init better

--- src/app/_components/Layout.tsx
+++ src/app/_components/Layout.tsx
@@ -1,22 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet, ScrollRestoration } from 'react-router-dom';
 
 import { DataProvider } from '../../dataSource/DataContext/DataContext';
-import { NavigationEvents } from './NavigationEvents';
+import { initI18n } from '../../dataSource/LocaleContext/i18n';
 
+import { NavigationEvents } from './NavigationEvents';
 import { hideLoader } from '../../utils/ui';
 
 export default function Layout() {
+  const [loadingI18n, setLoadingI18n] = useState(true);
+
   useEffect(() => {
-    void hideLoader();
+    initI18n()
+      .catch(console.error)
+      .finally(() => setLoadingI18n(false));
   }, []);
+
+  useEffect(() => {
+    if (!loadingI18n) {
+      // We are not hiding the loader on purpose, because change of loadingI18n to
+      // false will also lead to rendering the Outlet component, which may be the reason
+      // of layout shifts (due to loading images, for example).
+      const timeoutId = setTimeout(hideLoader, 100);
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+  }, [loadingI18n]);
 
   return (
     <>
       <NavigationEvents/>
       <ScrollRestoration/>
       <DataProvider>
-        <Outlet/>
+        {!loadingI18n && <Outlet/>}
       </DataProvider>
     </>
   );
--- src/dataSource/LocaleContext/i18n.ts
+++ src/dataSource/LocaleContext/i18n.ts
@@ -4,92 +4,30 @@ import HttpBackend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 import { getItemFromStorage } from '../../hooks/hooks';
 
-export const enum LOCALES {
-  EN = 'en',
-  RU = 'ru',
-  FA = 'fa',
-  UK = 'uk',
-  VI = 'vi',
-  TR = 'tr',
-  ID = 'id',
-  UZ = 'uz',
-  ES = 'es',
-  PT = 'pt',
-  FR = 'fr',
-  DE = 'de',
-  ZH = 'zh',
-}
-
-export const locales = {
-  [LOCALES.EN]: {
-    title: 'English',
-    localName: 'English',
-  },
-  [LOCALES.RU]: {
-    title: 'Russian',
-    localName: 'Русский',
-  },
-  [LOCALES.FA]: {
-    title: 'Farsi',
-    localName: 'فارسی',
-  },
-  [LOCALES.UK]: {
-    title: 'Ukrainian',
-    localName: 'Українська',
-  },
-  [LOCALES.VI]: {
-    title: 'Vietnamese',
-    localName: 'Tiếng Việt',
-  },
-  [LOCALES.TR]: {
-    title: 'Turkish',
-    localName: 'Türkçe',
-  },
-  [LOCALES.ID]: {
-    title: 'Indonesian',
-    localName: 'Bahasa Indonesia',
-  },
-  [LOCALES.UZ]: {
-    title: 'Uzbek',
-    localName: 'O‘zbek',
-  },
-  [LOCALES.ES]: {
-    title: 'Spanish',
-    localName: 'Español',
-  },
-  [LOCALES.PT]: {
-    title: 'Portuguese',
-    localName: 'Português',
-  },
-  [LOCALES.FR]: {
-    title: 'French',
-    localName: 'Français',
-  },
-  [LOCALES.DE]: {
-    title: 'German',
-    localName: 'Deutsch',
-  },
-  [LOCALES.ZH]: {
-    title: 'Chinese',
-    localName: '中文',
-  },
-};
-
-type I18nCallback = (locale: string) => void;
-
 export function initI18n() {
-  i18n
-    .use(HttpBackend)
+  return i18n
     .use(initReactI18next)
+    .use(HttpBackend)
     .use({
       type: 'languageDetector',
       async: true,
-      detect: async function (callback: I18nCallback) {
-        const localeFromGlobal = getLanguageCodeFromStartParams();
-        const res = await getItemFromStorage<string>('locale').catch(() => null);
-        const locale = res || localeFromGlobal || LOCALES.EN;
-
-        callback(locale);
+      async detect(callback: (locale: string) => void) {
+        Promise
+          .race([
+            getItemFromStorage<string>('locale'),
+            new Promise<never>((_, rej) => {
+              setTimeout(rej, 1000);
+            }),
+          ])
+          .catch(() => {
+            const codeFromGlobal = window?.Telegram?.WebApp?.initDataUnsafe?.user?.language_code;
+            return codeFromGlobal && [
+              'en', 'ru', 'es', 'fa', 'fr', 'hi', 'ko', 'ru', 'zh',
+            ].includes(codeFromGlobal)
+              ? codeFromGlobal
+              : 'en';
+          })
+          .then(callback);
       },
     })
     .init({
@@ -98,12 +36,4 @@ export function initI18n() {
         loadPath: '/locales/{{lng}}.json',
       },
     });
-}
-
-function getLanguageCodeFromStartParams() {
-  const codeFromGlobal = window?.Telegram?.WebApp?.initDataUnsafe?.user?.language_code;
-
-  if (codeFromGlobal && Object.keys(locales).includes(codeFromGlobal)) {
-    return codeFromGlobal;
-  }
 }
