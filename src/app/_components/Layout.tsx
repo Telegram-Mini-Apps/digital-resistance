e20bd9fe57ebc30ead5c225d1f95bfe3658570d3
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-26 17:53:07 +0500

    Add petitions request in layout

--- src/app/_components/Layout.tsx
+++ src/app/_components/Layout.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Outlet, ScrollRestoration } from 'react-router-dom';
+import useSWR from 'swr';
 
 import { DataProvider } from '../../dataSource/DataContext/DataContext';
 import { initI18n } from '../../dataSource/LocaleContext/i18n';
@@ -7,6 +8,32 @@ import { initI18n } from '../../dataSource/LocaleContext/i18n';
 import { hideLoader } from '../../utils/ui';
 
 export default function Layout() {
+  const {
+    data,
+    isLoading: isPetitionLoading,
+  } = useSWR<{
+    id: string;
+    title: string;
+    image_url: string;
+    description: string;
+    created_at: string;
+    is_signed_by_user: boolean;
+    signatures_count: number;
+  }>('petition', () => {
+    return fetch('/api/petitions/0de4e94f-f131-49d1-8d61-17b107e36793', {
+      headers: {
+        'x-init-data': window.Telegram.WebApp.initData,
+      },
+    })
+      .then(r => r.json())
+      .then(r => {
+        if (r.error) {
+          throw new Error(r.message);
+        }
+        return r.data;
+      });
+  });
+
   const [loadingI18n, setLoadingI18n] = useState(true);
 
   useEffect(() => {
@@ -16,7 +43,7 @@ export default function Layout() {
   }, []);
 
   useEffect(() => {
-    if (!loadingI18n) {
+    if (!loadingI18n && !isPetitionLoading && data) {
       // We are not hiding the loader on purpose, because change of loadingI18n to
       // false will also lead to rendering the Outlet component, which may be the reason
       // of layout shifts (due to loading images, for example).
@@ -25,14 +52,16 @@ export default function Layout() {
         clearTimeout(timeoutId);
       };
     }
-  }, [loadingI18n]);
+  }, [loadingI18n, isPetitionLoading, data]);
 
   return (
     <>
       <ScrollRestoration/>
-      <DataProvider>
-        {!loadingI18n && <Outlet/>}
-      </DataProvider>
+      {data && (
+        <DataProvider isSigned={data.is_signed_by_user} signaturesCount={data.signatures_count}>
+          {!loadingI18n && <Outlet/>}
+        </DataProvider>
+      )}
     </>
   );
 }
