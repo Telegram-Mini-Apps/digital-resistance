9cc83beeaba157b5b579f92dd4ebdbb3d9202932
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-26 17:53:34 +0500

    Cleanup DataContext

--- src/dataSource/DataContext/DataContext.tsx
+++ src/dataSource/DataContext/DataContext.tsx
@@ -1,80 +1,25 @@
-import React, { PropsWithChildren } from 'react';
-import { useLocation } from 'react-router-dom';
-import { BlockData } from '../../../types/Blocks';
-import { TWAApplication, TWACategory } from '../../../types/entities';
+import React, { type PropsWithChildren } from 'react';
+
 import { useMixpanel } from '../../hooks/hooks';
-import { reorderApps, useActiveCategory } from './utils';
 
-interface IDataContextProps {
-  categories: Array<TWACategory>;
-  appsRanking: Array<number>,
-  applications: Array<TWAApplication>;
-  visibleApplications: Array<TWAApplication>;
-  activeCategory?: TWACategory;
-  mainPageBlockConfig?: Array<BlockData>;
-  navigationConfig?: Array<TWACategory>;
-  setActiveCategory: (category: TWACategory) => void;
-  getActiveCategory?: () => TWACategory | void;
+interface DataContextState {
+  signaturesCount: number;
+  isSigned: boolean;
 }
 
-interface IDataProviderProps {
-  categories?: Array<TWACategory>;
-  appsRanking?: Array<number>;
-  applications?: Array<TWAApplication>;
-  mainPageBlockConfig?: Array<BlockData>;
-  navigationConfig?: Array<TWACategory>;
-}
-
-const DataContext = React.createContext<IDataContextProps>({
-  categories: [],
-  appsRanking: [],
-  applications: [],
-  visibleApplications: [],
-  mainPageBlockConfig: [],
-  navigationConfig: [],
-  setActiveCategory: () => void 0,
+const DataContext = React.createContext<DataContextState>({
+  signaturesCount: 0,
+  isSigned: false,
 });
 
-export const DataProvider: React.FC<PropsWithChildren<IDataProviderProps>> = (props) => {
+export function DataProvider(props: PropsWithChildren<DataContextState>) {
   useMixpanel();
 
-  const location = useLocation();
-
-  const {
-    categories = [],
-    appsRanking = [],
-    applications = [],
-    navigationConfig = [],
-    mainPageBlockConfig = [],
-  } = props;
-
-  const {
-    activeCategory,
-    setActiveCategory,
-    getActiveCategory,
-  } = useActiveCategory(location.pathname, categories);
-
-  const visibleApplications = React.useMemo(() => {
-    return reorderApps(applications, appsRanking);
-  }, [activeCategory, applications, appsRanking]);
-
-  const contextValue = {
-    categories,
-    appsRanking,
-    applications,
-    activeCategory,
-    navigationConfig,
-    getActiveCategory,
-    setActiveCategory,
-    visibleApplications,
-    mainPageBlockConfig,
-  };
-
   return (
-    <DataContext.Provider value={contextValue}>
+    <DataContext.Provider value={props}>
       {props.children}
     </DataContext.Provider>
   );
-};
+}
 
 export const useDataContext = () => React.useContext(DataContext);
