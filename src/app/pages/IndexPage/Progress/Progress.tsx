c95486f884e645de5c2e1a309e7c4c4667f3f0d0
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-26 22:46:38 +0500

    Correctly compute target signs count

--- src/app/pages/IndexPage/Progress/Progress.tsx
+++ src/app/pages/IndexPage/Progress/Progress.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import cn from 'classnames';
 
 import { Section } from '../Section/Section';
@@ -6,15 +6,18 @@ import { Section } from '../Section/Section';
 import styles from './Progress.module.scss';
 import { Trans } from 'react-i18next';
 
-export function Progress({ current, next }: { current: number; next: number }) {
+export function Progress({ current }: { current: number }) {
   const formatNumber = useCallback((num: number) => {
     return num.toLocaleString('en-US');
   }, []);
+  const targetCount = useMemo(() => {
+    return [1_000_000, 100_000_000, 500_000_000].find(m => m > current) || current;
+  }, [current]);
 
   return (
     <Section className={styles.root}>
       <div className={styles.bar}>
-        <div className={styles.barThumb} style={{ width: `${current / next * 100}%` }}/>
+        <div className={styles.barThumb} style={{ width: `${current / targetCount * 100}%` }}/>
       </div>
       <div className={styles.captions}>
         <div className={styles.item}>
@@ -25,7 +28,7 @@ export function Progress({ current, next }: { current: number; next: number }) {
         </div>
 
         <div className={cn(styles.item, styles.itemRight)}>
-          <div className={styles.itemTitle}>{formatNumber(next)}</div>
+          <div className={styles.itemTitle}>{formatNumber(targetCount)}</div>
           <div className={styles.itemSubtitle}>
             <Trans i18nKey={'next_goal'}/>
           </div>
