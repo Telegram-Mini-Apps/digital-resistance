1caa1a1fed3eb2f5827c3a8be97a7bcb1063b4ba
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-08-27 14:19:01 +0500

    Fix invalid language detection

--- src/dataSource/LocaleContext/i18n.ts
+++ src/dataSource/LocaleContext/i18n.ts
@@ -2,7 +2,6 @@ import i18n from 'i18next';
 
 import HttpBackend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
-import { getItemFromStorage } from '../../hooks/hooks';
 
 export function initI18n() {
   return i18n
@@ -11,23 +10,15 @@ export function initI18n() {
     .use({
       type: 'languageDetector',
       async: true,
-      async detect(callback: (locale: string) => void) {
-        Promise
-          .race([
-            getItemFromStorage<string>('locale'),
-            new Promise<never>((_, rej) => {
-              setTimeout(rej, 1000);
-            }),
-          ])
-          .catch(() => {
-            const codeFromGlobal = window?.Telegram?.WebApp?.initDataUnsafe?.user?.language_code;
-            return codeFromGlobal && [
-              'en', 'ru', 'es', 'fa', 'fr', 'hi', 'ko', 'ru', 'zh',
-            ].includes(codeFromGlobal)
-              ? codeFromGlobal
-              : 'en';
-          })
-          .then(callback);
+      detect(callback: (locale: string) => void) {
+        const codeFromGlobal = window?.Telegram?.WebApp?.initDataUnsafe?.user?.language_code;
+        callback(
+          codeFromGlobal && [
+            'en', 'ru', 'es', 'fa', 'fr', 'hi', 'ko', 'ru', 'zh',
+          ].includes(codeFromGlobal)
+            ? codeFromGlobal
+            : 'en',
+        );
       },
     })
     .init({
